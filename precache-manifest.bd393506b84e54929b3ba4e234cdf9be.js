fd4de7105fe944048b9f9ae45ae063014d88a5a1
hoojianliang <hoojianliang@users.noreply.github.com>
2019-10-27 12:50:00 +0800

    Updates

---
+++ precache-manifest.bd393506b84e54929b3ba4e234cdf9be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e83f1bbe45caf4ff50e409cf768f925f",
+    "url": "/rigel-game/index.html"
+  },
+  {
+    "revision": "ff5585c5440afc7ca57a",
+    "url": "/rigel-game/static/css/main.1defee68.chunk.css"
+  },
+  {
+    "revision": "6e3263e5e1817e5efaeb",
+    "url": "/rigel-game/static/js/2.9562fb46.chunk.js"
+  },
+  {
+    "revision": "ff5585c5440afc7ca57a",
+    "url": "/rigel-game/static/js/main.9eecf79f.chunk.js"
+  },
+  {
+    "revision": "8530e672b0471849f679",
+    "url": "/rigel-game/static/js/runtime-main.0288f356.js"
+  }
+]);
